61ed737906e0341dc1ba0ac5fd86380d7f9d0429
Gen Takahashi <gendosu@gmail.com>
2016-09-02 17:44:16 +0900

    redisを使ってtopを表示出来るようにする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,7 @@
 class TopController < ApplicationController
   def index
+    redis = Redis.new(:url => ENV['REDIS_URL'])
+    redis.set("mykey", "hello world")
+    @result = redis.get("mykey")
   end
 end
